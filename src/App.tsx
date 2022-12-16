958e825820f63769dab8d6edf79358226de97540
koichiro <koichiro.world@gmail.com>
2022-12-16 22:12:40 +0900

    test9

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,31 @@
 import React from "react";
-import { withAuthenticator, AmplifySignOut } from "@aws-amplify/ui-react";
-import Amplify, { Auth } from "aws-amplify";
+import logo from "./logo.svg";
+import "./App.css";
+import { withAuthenticator } from "@aws-amplify/ui-react";
+import "@aws-amplify/ui-react/styles.css"; // 認証画面用のCSS
+import { Amplify } from "@aws-amplify/core";
+import { Auth } from "@aws-amplify/auth";
 import awsconfig from "./aws-exports";
 
 Amplify.configure(awsconfig);
 Auth.configure(awsconfig);
 
-const App = () => (
-  <div>
-    <AmplifySignOut />
-    My App
-  </div>
-);
-
+function App({ signOut, user }: any) {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h2>Hello React app with AWS</h2>
+        {user ? (
+          <>
+            <h3>私は権限を持っています:{user.username}</h3>
+            <button onClick={signOut}>サインアウト</button>
+          </>
+        ) : (
+          <h3>権限がありません</h3>
+        )}
+      </header>
+    </div>
+  );
+}
 export default withAuthenticator(App);
