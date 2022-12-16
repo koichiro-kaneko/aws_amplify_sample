f4c2e40d64a23773aba34d86e4941063dfac62db
koichiro <koichiro.world@gmail.com>
2022-12-16 20:15:51 +0900

    test6

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import { Amplify } from "aws-amplify";
+import { Amplify, Auth } from "aws-amplify";
 import { withAuthenticator } from "@aws-amplify/ui-react";
 import "@aws-amplify/ui-react/styles.css"; // 認証画面用のCSS
 import awsExports from "./aws-exports";
 Amplify.configure(awsExports);
+Auth.configure(awsExports);
 
 function App({ signOut, user }: any) {
   return (
